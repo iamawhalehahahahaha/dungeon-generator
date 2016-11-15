a4be8196467b1165f31cb83e212f55b0c51aa673
John West <iamawhalehahahahaha@gmail.com>
2016-11-15 09:33:36 -0600

    This is interesting, I'm getting this (I hope).

--- include/Dungeon.h
+++ include/Dungeon.h
@@ -1,10 +1,12 @@
-#ifndef DUNGEON_GEN_H
-#define DUNGEON_GEN_H
+#ifndef DUNGEON_H
+#define DUNGEON_H
 
 #include <cstddef>
 
 #define DEFAULT_LENGTH (20)
 #define DEFAULT WIDTH (20)
+#define ROOM_WIDTH_MAX (4)
+#define ROOM_LENGTH_MAX (4)
 
 class Dungeon
 {
@@ -12,12 +14,16 @@ public:
     Dungeon();
     Dungeon(std::size_t length, std::size_t width);
     ~Dungeon();
+
 private:
     class room_t
     {
-    
+    public:
+    private:
     }
+
     void gen();
     makeRoom();
 }
-#endif /* DUNGEON_GEN_H */
+
+#endif /* DUNGEON_H */
